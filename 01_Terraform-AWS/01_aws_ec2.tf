616f6e05305f0d72fb03aa56f39ea7d3aa18feb0
Dinesh <dinesh30mane@gmail.com>
2025-05-13 18:28:58 +0530

    updated tf script to create a ec2 instance on aws cloud

--- 01_Terraform-AWS/01_aws_ec2.tf
+++ 01_Terraform-AWS/01_aws_ec2.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.0.0-beta1"
+    }
+  }
+}
 
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "basic_ec2" {
+  ami           = "ami-0c02fb55956c7d316" 
+  instance_type = "t3.micro"              
+  key_name      = "instance-cha-naav"                
+
+  tags = {
+    Name = "BasicEC2Instance"
+  }
+}
